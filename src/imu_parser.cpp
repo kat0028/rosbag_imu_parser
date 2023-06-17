6a31e1cab6354d721130cf6268f9621d15c91dbc
Kyle Thompson <kat0028@auburn.edu>
2023-06-17 09:54:04 -0500

    added error checking for bags with multiple messages

--- src/imu_parser.cpp
+++ src/imu_parser.cpp
@@ -12,7 +12,7 @@ int main()
 
     // Configuration Variables -------------------------------------------------------------------
     std::string path = "/home/kyle/Data/rosbag/";
-    std::string bag_filename = path + "nick_hg1700ag75_syncIntern_2023-04-20-16-15-24.bag";
+    std::string bag_filename = path + "june_15_hg1700_long_duration_2023-06-15-16-58-48.bag";
     std::string csv_filename = path + "data.csv";
     // -------------------------------------------------------------------------------------------
 
@@ -30,8 +30,8 @@ int main()
 
     sensor_msgs::Imu::ConstPtr msg;
 
-    int header_stamp_sec;
-    int header_stamp_nsec;
+    double header_stamp_sec;
+    double header_stamp_nsec;
     double time;
     double angular_velocity_x;
     double angular_velocity_y;
@@ -47,27 +47,29 @@ int main()
     output_line = "time,gx,gy,gz,ax,ay,az\n";
     csv_file << output_line;
 
-    for(rosbag::MessageInstance const m: rosbag::View(bag))
+    for (rosbag::MessageInstance const m : rosbag::View(bag))
     {
-        msg  = m.instantiate<sensor_msgs::Imu>();
+        msg = m.instantiate<sensor_msgs::Imu>();
+        if (msg != NULL)
+        {
+            header_stamp_sec = msg->header.stamp.sec;
+            header_stamp_nsec = msg->header.stamp.nsec;
+            time = header_stamp_sec + header_stamp_nsec * pow(10, -9);
+            angular_velocity_x = msg->angular_velocity.x;
+            angular_velocity_y = msg->angular_velocity.y;
+            angular_velocity_z = msg->angular_velocity.z;
+            linear_acceleration_x = msg->angular_velocity.x;
+            linear_acceleration_y = msg->angular_velocity.y;
+            linear_acceleration_z = msg->angular_velocity.z;
 
-        header_stamp_sec = msg->header.stamp.sec;
-        header_stamp_nsec = msg->header.stamp.nsec;
-        time = header_stamp_sec + header_stamp_nsec * pow(10, -9);
-        angular_velocity_x = msg->angular_velocity.x;
-        angular_velocity_y = msg->angular_velocity.y;
-        angular_velocity_z = msg->angular_velocity.z;
-        linear_acceleration_x = msg->angular_velocity.x;
-        linear_acceleration_y = msg->angular_velocity.y;
-        linear_acceleration_z = msg->angular_velocity.z;
-
-        output_line = std::to_string(time) + ',' + std::to_string(angular_velocity_x) + ',' + std::to_string(angular_velocity_y) + ',' + std::to_string(angular_velocity_z) + ',' + std::to_string(linear_acceleration_x) + ',' + std::to_string(linear_acceleration_y) + ',' + std::to_string(linear_acceleration_z) + '\n';
-        csv_file << output_line;        
+            output_line = std::to_string(time) + ',' + std::to_string(angular_velocity_x) + ',' + std::to_string(angular_velocity_y) + ',' + std::to_string(angular_velocity_z) + ',' + std::to_string(linear_acceleration_x) + ',' + std::to_string(linear_acceleration_y) + ',' + std::to_string(linear_acceleration_z) + '\n';
+            csv_file << output_line;
+        }
     }
 
     csv_file.close();
 
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << "Run time: " << std::chrono::duration_cast<std::chrono::seconds>(end-begin).count() << " seconds" << std::endl;
+    std::cout << "Run time: " << std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() << " seconds" << std::endl;
     return 0;
 }
